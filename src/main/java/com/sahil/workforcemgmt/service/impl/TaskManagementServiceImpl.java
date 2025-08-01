70e6507771ea500ceb441c5f3c43f6086324c538
SahilAnower <sahilanower2@gmail.com>
2025-08-02 03:21:25 +0530

    Added logic to filter out cancelled tasks

--- src/main/java/com/sahil/workforcemgmt/service/impl/TaskManagementServiceImpl.java
+++ src/main/java/com/sahil/workforcemgmt/service/impl/TaskManagementServiceImpl.java
@@ -111,9 +111,8 @@ public class TaskManagementServiceImpl implements TaskManagementService {
     public List<TaskManagementDto> fetchTasksByDate(TaskFetchByDateRequest request) {
         List<TaskManagement> tasks = taskRepository.findByAssigneeIdIn(request.getAssigneeIds());
 
-
-        // BUG #2 is here. It should filter out CANCELLED tasks but doesn't.
         List<TaskManagement> filteredTasks = tasks.stream()
+                .filter(t -> t.getStatus() != TaskStatus.CANCELLED)
                 .filter(task -> {
                     // This logic is incomplete for the assignment.
                     // It should check against startDate and endDate.
